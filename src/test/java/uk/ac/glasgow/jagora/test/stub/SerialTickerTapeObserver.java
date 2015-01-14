e1c9b4f205617e7f07afd07ca0ddeda3ebf54fc5
tws <tws@ef8dee83-5f3d-43c0-baf0-9562671df143>
2015-01-14 14:21:12 +0000

    Tidied up imports.
    git-svn-id: http://macdonald.dcs.gla.ac.uk/~tws/repositories/jagora/trunk@23 ef8dee83-5f3d-43c0-baf0-9562671df143

--- src/test/java/uk/ac/glasgow/jagora/test/stub/SerialTickerTapeObserver.java
+++ src/test/java/uk/ac/glasgow/jagora/test/stub/SerialTickerTapeObserver.java
@@ -1,15 +1,8 @@
 package uk.ac.glasgow.jagora.test.stub;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import uk.ac.glasgow.jagora.Stock;
-import uk.ac.glasgow.jagora.Trade;
 import uk.ac.glasgow.jagora.ticker.TickerTapeListener;
-import uk.ac.glasgow.jagora.ticker.TickerTapeObserver;
 import uk.ac.glasgow.jagora.ticker.TradeExecutionEvent;
 import uk.ac.glasgow.jagora.ticker.impl.AbstractTickerTapeObserver;
-import uk.ac.glasgow.jagora.world.TickEvent;
 
 public class SerialTickerTapeObserver extends AbstractTickerTapeObserver {
 	
