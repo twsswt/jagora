d8adc7d96878c2f37fe95966eec18f4ccae3c92f
ivelin <ivelin@ef8dee83-5f3d-43c0-baf0-9562671df143>
2015-06-26 13:34:07 +0000

    Improved API documentation
    git-svn-id: http://macdonald.dcs.gla.ac.uk/~tws/repositories/jagora/branches/ivo-summer-2015@62 ef8dee83-5f3d-43c0-baf0-9562671df143

--- api/src/main/java/uk/ac/glasgow/jagora/MarketFactory.java
+++ api/src/main/java/uk/ac/glasgow/jagora/MarketFactory.java
@@ -3,5 +3,5 @@ package uk.ac.glasgow.jagora;
 import uk.ac.glasgow.jagora.world.World;
 
 public interface MarketFactory {
-	public Market createOrderDrivenMarket(Stock stock, World world);
+	Market createOrderDrivenMarket(Stock stock, World world);
 }
